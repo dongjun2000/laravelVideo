03361be12e29062463d9d35607f106761dcb1a5b
董俊 <418826102@qq.com>
2019-09-09 16:32:29 +0800

    使用数据迁移构建课程和视频数据表

---
+++ database/migrations/2019_09_09_113537_create_lessons_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLessonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('title');
+            $table->string('intro');
+            $table->string('preview');
+            $table->tinyInteger('iscommend')->default(0);
+            $table->tinyInteger('ishot')->default(0);
+            $table->integer('click')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lessons');
+    }
+}
